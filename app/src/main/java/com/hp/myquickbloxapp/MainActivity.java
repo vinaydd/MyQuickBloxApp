217634ea1ae89a8d89988fddbcaf1af836eb31cf
vinay <vinay@innocirc.com>
2020-04-15 10:37:20 +0530

    add permmission check

--- app/src/main/java/com/hp/myquickbloxapp/MainActivity.java
+++ app/src/main/java/com/hp/myquickbloxapp/MainActivity.java
@@ -1,21 +1,56 @@
 package com.hp.myquickbloxapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public class MainActivity extends CommonBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        checkPremittion();
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startCallnew(true);
             }
         });
+    }
+
+
+
+    private void checkPremittion() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_CONTACTS,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_CONTACTS,
+                        Manifest.permission.CALL_PHONE,
+                        Manifest.permission.RECEIVE_SMS,
+                        Manifest.permission.RECORD_AUDIO
+                ).withListener(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {/* ... */}
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+            }
+
+
+        }).check();
     }
 
     @Override
